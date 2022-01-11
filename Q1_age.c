0ebc63640b8eab2d3169ad9cc3cee42a7dd196ff
adoocavo <97431935+adoocavo@users.noreply.github.com>
2022-01-11 21:29:24 +0900

    Part01_ch1
    2022_C언어 특강 Part01_ch1 실습 source code

---
+++ Q1_age.c
@@ -0,0 +1,7 @@
+/* 12/28과제 Q1-7 */
+#include<stdio.h>
+int main(void) {   //같은 프로젝트 내에 다른 소스코드 중애서 main함수 있는지 확인!!(실행파일 실행 시 main함수는 하나여야 한다)
+	printf("my age is %d.\nmy brother age is %d ", 25, 22);
+
+	return 0;
+}
